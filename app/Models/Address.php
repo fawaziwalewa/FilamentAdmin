7a586de533671c23d978f50c044591b9498e2752
fawaziwalewa <wizardalani267@gmail.com>
2022-12-31 14:16:26 +0100

    version 1.0.5
    Fixed address bug on Brands and Customers

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -9,4 +9,12 @@ class Address extends Model
 {
     use HasFactory;
     protected $fillable = ['street', 'zip', 'city', 'state', 'country'];
+
+    public function customers(){
+        return $this->belongsToMany(Customer::class, 'customer_addresses');
+    }
+
+    public function brands(){
+        return $this->belongsToMany(Brand::class, 'brand_addresses');
+    }
 }
